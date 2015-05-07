ab1793e8a364685599016c6dd63ec637929c2bbd
fsl <anon@example.net>
2015-05-07 17:28:44 +0200

    simple-history adjustments

--- wp-content/plugins/simple-history/loggers/SimpleLogger.php
+++ wp-content/plugins/simple-history/loggers/SimpleLogger.php
@@ -182,7 +182,6 @@ class SimpleLogger {
 						$tmpl_initiator_html = '
 							<a href="%6$s" class="SimpleHistoryLogitem__headerUserProfileLink">
 								<strong class="SimpleHistoryLogitem__inlineDivided">%3$s</strong>
-								<span class="SimpleHistoryLogitem__inlineDivided SimpleHistoryLogitem__headerEmail">%2$s</span>
 							</a>
 						';
 
